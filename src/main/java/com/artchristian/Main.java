9ef5ee4746434573d08cdbb9f163de4428e52f14
artchristian <artchristiangene@gmail.com>
2024-11-26 14:47:13 +0800

    Created a Customer model in Main.java

--- src/main/java/com/artchristian/Main.java
+++ src/main/java/com/artchristian/Main.java
@@ -2,11 +2,10 @@ package com.artchristian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController
@@ -14,6 +13,77 @@ public class Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+	}
+
+	class Customer {
+		private Integer id;
+		private String name;
+		private String email;
+		private Integer age;
+
+		public Customer(Integer id, String name, String email, Integer age) {
+			this.id = id;
+			this.name = name;
+			this.email = email;
+			this.age = age;
+		}
+
+		public Customer() {
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public Integer getAge() {
+			return age;
+		}
+
+		public void setAge(Integer age) {
+			this.age = age;
+		}
+
+		@Override
+		public String toString() {
+			return "Customer{" +
+					"id=" + id +
+					", name='" + name + '\'' +
+					", email='" + email + '\'' +
+					", age=" + age +
+					'}';
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (o == null || getClass() != o.getClass()) return false;
+			Customer customer = (Customer) o;
+			return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(id, name, email, age);
+		}
 	}
 
 }
