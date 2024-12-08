3200bc793a0a6409746266c37b65c469c360d844
Art <artchristiangene@gmail.com>
2024-12-09 02:05:55 +0800

    JDBC Implementation of CustomerDataAccess Service selectAll, selectById, Insert

---
+++ src/main/java/com/artchristian/customer/CustomerJDBCDataAccessService.java
@@ -0,0 +1,75 @@
+package com.artchristian.customer;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("jdbc")
+public class CustomerJDBCDataAccessService implements CustomerDao{
+
+    private final JdbcTemplate jdbcTemplate;
+    private final CustomerRowMapper customerRowMapper;
+
+    public CustomerJDBCDataAccessService(JdbcTemplate jdbcTemplate, CustomerRowMapper customerRowMapper) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.customerRowMapper = customerRowMapper;
+    }
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        var sql = """
+                SELECT id, name, email, age
+                FROM customer
+                """;
+        return jdbcTemplate.query(sql, customerRowMapper);
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer id) {
+        var sql = """
+                SELECT id, name, email, age
+                FROM customer
+                WHERE id = ?
+                """;
+        return jdbcTemplate.query(sql,customerRowMapper, id)
+                .stream()
+                .findFirst();
+    }
+
+    @Override
+    public void deleteCustomer(Integer id) {
+
+    }
+
+    @Override
+    public void insertCustomer(Customer customer) {
+        var sql = """
+               INSERT INTO customer(name, email, age)
+               VALUES (?, ?, ?)
+                """;
+        int result = jdbcTemplate.update(
+                sql,
+                customer.getName(),
+                customer.getEmail(),
+                customer.getAge()
+        );
+        System.out.println("jdbcTemplate.update = " + result);
+    }
+
+    @Override
+    public boolean existsPersonWithEmail(String email) {
+        return false;
+    }
+
+    @Override
+    public boolean existsPersonWithID(Integer id) {
+        return false;
+    }
+
+    @Override
+    public void updateCustomer(Customer customer) {
+
+    }
+}
