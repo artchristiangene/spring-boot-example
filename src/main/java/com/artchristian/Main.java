cd688767c60a4058ca8c02b76c95f54cb0014bc7
Art <artchristiangene@gmail.com>
2024-12-09 01:16:30 +0800

    Added JavaFaker resource

--- src/main/java/com/artchristian/Main.java
+++ src/main/java/com/artchristian/Main.java
@@ -2,12 +2,15 @@ package com.artchristian;
 
 import com.artchristian.customer.Customer;
 import com.artchristian.customer.CustomerRepository;
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
+import java.util.Random;
 
 @SpringBootApplication
 public class Main {
@@ -19,20 +22,17 @@ public class Main {
 	@Bean
 	CommandLineRunner runner(CustomerRepository customerRepository){
 		return args -> {
-			Customer alex = new Customer(
-					1,
-					"Art",
-					"art@gmail.com",
-					33
+			var faker = new Faker();
+			Random random = new Random();
+			Name name = faker.name();
+			String firstName = name.firstName();
+			String lastName = name.lastName();
+			Customer customer = new Customer(
+					firstName + " " + lastName,
+					firstName.toLowerCase() + "." + lastName.toLowerCase() + "@example.com",
+					random.nextInt(18, 99)
 			);
-			Customer mina = new Customer(
-					2,
-					"Mina",
-					"mina@gmail.com",
-					32
-			);
-			List<Customer> customers = List.of(alex, mina);
-			customerRepository.saveAll(customers);
+			customerRepository.save(customer);
 		};
 	}
 }
