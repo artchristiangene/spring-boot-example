74b09b7f0621ae0a72a3d898b9128cd676ce331a
artchristian <artchristiangene@gmail.com>
2025-01-08 16:52:38 +0800

    created test class for CustomerJDBCDataAccessService Class

--- src/test/java/com/artchristian/TestContainersTest.java
+++ src/test/java/com/artchristian/TestContainersTest.java
@@ -1,36 +1,15 @@
 package com.artchristian;
 
-import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
-public class TestContainersTest {
 
-    @Container
-    private static final PostgreSQLContainer<?> postgreSQLContainer =
-            new PostgreSQLContainer<>("postgres:14.1")
-                    .withDatabaseName("artchristiangene-dao-unit-test")
-                    .withUsername("artchristiangene")
-                    .withPassword("password");
+public class TestContainersTest extends AbstractTestContainer {
 
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
-    }
-
-    @Test
-    void canApplyDbMigrationWithFlyway() {
-        Flyway flyway = Flyway.configure().dataSource(
-                postgreSQLContainer.getJdbcUrl(),
-                postgreSQLContainer.getUsername(),
-                postgreSQLContainer.getPassword()
-        ).load();
-        flyway.migrate();
     }
 }
---
+++ src/test/java/com/artchristian/customer/CustomerJDBCDataAccessServiceTest.java
@@ -0,0 +1,134 @@
+package com.artchristian.customer;
+
+import com.artchristian.AbstractTestContainer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerJDBCDataAccessServiceTest extends AbstractTestContainer {
+
+    private CustomerJDBCDataAccessService underTest;
+    private CustomerRowMapper customerRowMapper = new CustomerRowMapper();
+
+    @BeforeEach
+    void setUp() {
+        underTest = new CustomerJDBCDataAccessService(
+                getJdbcTemplate(),
+                customerRowMapper
+        );
+    }
+
+    @Test
+    void selectAllCustomers() {
+        //Given
+        Customer customer = new Customer(
+            FAKER.name().fullName(),
+            FAKER.internet().emailAddress()+ "-" + UUID.randomUUID(),
+            20
+        );
+
+        underTest.insertCustomer(customer);
+
+        //When
+        List<Customer> actual = underTest.selectAllCustomers();
+
+        //Then
+        assertThat(actual).isNotEmpty();
+    }
+
+    @Test
+    void selectCustomerById() {
+        //Given
+        String email = FAKER.internet().emailAddress() + "-" + UUID.randomUUID();
+        Customer customer = new Customer(
+                FAKER.name().fullName(),
+                email,
+                20
+        );
+
+        underTest.insertCustomer(customer);
+
+        int id = underTest.selectAllCustomers()
+                .stream()
+                .filter(c -> c.getEmail().equals(email))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+
+        //When
+
+        Optional<Customer> actual = underTest.selectCustomerById(id);
+
+        //Then
+        assertThat(actual).isPresent().hasValueSatisfying(c ->{
+            assertThat(c.getId()).isEqualTo(id);
+            assertThat(c.getName()).isEqualTo(customer.getName());
+            assertThat(c.getEmail()).isEqualTo(customer.getEmail());
+            assertThat(c.getAge()).isEqualTo(customer.getAge());
+        });
+    }
+
+    @Test
+    void willReturnEmptyWhenSelectCustomerById(){
+        //Given
+        int id = -1;
+
+        //When
+        Optional<Customer> actual = underTest.selectCustomerById(id);
+
+        //Then
+        assertThat(actual).isEmpty();
+    }
+
+    @Test
+    void deleteCustomer() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void insertCustomer() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void existsPersonWithEmail() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void existsPersonWithID() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void updateCustomer() {
+        //Given
+
+        //When
+
+        //Then
+    }
+}
