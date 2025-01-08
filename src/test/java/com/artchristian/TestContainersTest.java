0175f52e5f395cdb2d23facd0d7ea8e7e8417af5
artchristian <artchristiangene@gmail.com>
2025-01-08 13:30:31 +0800

    added test method canApplyDbMigrationWithFlyway

--- src/test/java/com/artchristian/TestContainersTest.java
+++ src/test/java/com/artchristian/TestContainersTest.java
@@ -1,5 +1,6 @@
 package com.artchristian;
 
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -21,5 +22,15 @@ public class TestContainersTest {
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
+    }
+
+    @Test
+    void canApplyDbMigrationWithFlyway() {
+        Flyway flyway = Flyway.configure().dataSource(
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword()
+        ).load();
+        flyway.migrate();
     }
 }
